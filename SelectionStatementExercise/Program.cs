684d8a1a977317b47037cf9669a17be31483a3ab
Maverick Grayer <maverick72032@gmail.com>
2023-11-07 14:46:16 -0500

    assignment 1 complete

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            Console.WriteLine("Try to guess my favorite number! It is between 1 and 1000.");
+            while (true)
+            {
+                var userInput = int.Parse(Console.ReadLine());
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine("Too low!");
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine("Too high");
+                }
+                else
+                {
+                    Console.WriteLine("Nice, you got it!");
+                    break;
+                }
+            }
         }
     }
 }
